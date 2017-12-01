cd3d23166ccf9e48af57334d27a5a15cdc3fdf8f
hoff0899 <hoff0899@morris.umn.edu>
2017-12-01 16:50:23 -0600

    Create functions for getting current event of the day Remove top bar on html Start implementation of getting results of choices from events

--- public/click.js
+++ public/click.js
@@ -7,11 +7,14 @@ function GameStateCtrl($scope,registerApi){
 
    $scope.classEvents=[]; //Stores eventID for the corresponding
    $scope.freeEvents=[];  //Event in the database
+   $scope.numberOfEvents=0;
    $scope.username="No one";
    $scope.password="N/A"
    //$scope.newCharacter=-1; //0 means they choose to login, 1 means they choose to make a new character
    $scope.characterInformation=characterInformation;
    $scope.characterInventory=characterInventory;
+   $scope.currentClassEvent=currentClassEvent;
+   $scope.currentFreeEvent=currentFreeEvent;
    $scope.userID=-1;
    $scope.isLoggedIn=false;
 
@@ -35,6 +38,20 @@ function GameStateCtrl($scope,registerApi){
 	   });
    }
 
+   function getCurrentClassEvent(){
+        loading=true;
+        $scope.errorMessage='';
+        var eventID = getRandomInt(0, ClassEvents.length);
+        GameStateApi.getCurrentClassEvent(eventID)
+           .success(function(data) {
+                   scope.currentClassEvent=data;
+                   loading=false;
+           })
+           .error(function(){
+                   $scope.errorMessage="Unable to get currentClassEvent: Database request failed";
+                   loading=false;
+           });
+   }
 
    function getFreeEvents() {
         loading=true;
@@ -49,6 +66,28 @@ function GameStateCtrl($scope,registerApi){
                    loading=false;
            });
    }
+
+   function getCurrentFreeEvent(){
+	loading=true;
+	$scope.errorMessage='';
+	var eventID = getRandomInt(0, freeEvents.length);
+	GameStateApi.getCurrentFreeEvent(eventID)
+	   .success(function(data) {
+		   scope.currentFreeEvent=data;
+		   loading=false;
+	   })
+	   .error(function(){
+		   $scope.errorMessage="Unable to get currentFreeEvent: Database request failed";
+		   loading=false;
+	   });
+   }
+
+   //Thanks internet
+   function getRandomInt(min, max) {
+ 	min = Math.ceil(min);
+  	max = Math.floor(max);
+  	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 
    function getCharacterInformation(userID) {
 	loading=true;
@@ -101,8 +140,24 @@ function GameStateCtrl($scope,registerApi){
 	  }
    }
 
+	//Write helper function that does the scanning (pass in button1, button2 etc depending on $event)
+   //function selectEventChoice($event) {
+	//$scope.errorMessage='';
+	//loading = true;
+//	if ($event == 1) {
+		
+//	}
+//	while(true){
+//		var counter = 0;
+//		while(currentFreeEvent.button
+//	}
+	
+  // }
+
    getClassEvents();
    getFreeEvents();
+	//Assuming we start with a class event
+   getCurrentClassEvent();
 }
 function gameStateApi($http,apiUrl){
   	return{
@@ -110,10 +165,16 @@ function gameStateApi($http,apiUrl){
       			var url = apiUrl + '/classEvents';
       			return $http.get(url);
     		},
+		getCurrentClassEvent: function(eventID){
+                        var url = apiUrl + '/getCurrentClassEvent?eventID='+eventID;
+                },
 		getFreeEvents: function(){
         		var url = apiUrl + '/freeEvents';
         		return $http.get(url);
     		},
+		getCurrentFreeEvent: function(eventID){
+			var url = apiUrl + '/getCurrentFreeEvent?eventID='+eventID;
+		},
 		getCharacterInformation: function(userID){
 			var url = apiUrl + '/getCharInfo?userID='+userID;
 			return $http.get(url);
