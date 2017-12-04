6cfdd7aa93b2be38faa5bc2933febc8cb3424908
Xaitheng Yang <yang3792@avenger.morris.umn.edu>
2017-12-04 17:02:16 -0600

    Added new result event functionality and newCharacter functionality updated Database to reflect these changes

--- express.js
+++ express.js
@@ -81,25 +81,44 @@ app.get("/update",function(req,res){
 //----------------------------------------------------------------------------------------------------------------------
 
 app.get("/newCharacter",function(req,res){
-	var invID = req.param('invID');
-        var quantity = req.param('quantity');
-        var receiptNumber = req.param('receiptNumber');
-        var user = req.param('user');
-        var firstTime = req.param('firstTime');
-        var lastTime = req.param('lastTime');
-        var finalCost = req.param('finalCost');
 
-	var sql = 'SELECT XaiMarsh.till_buttons.*, XaiMarsh.prices.prices FROM XaiMarsh.till_buttons LEFT JOIN (XaiMarsh.prices) on (XaiMarsh.till_buttons.invID = XaiMarsh.prices.id)';
+	var IDNumber = req.param('IDNumber');
+	var Firstname = req.param('Firstname');
+	var Lastname = req.param('Lastname');
+	var Overwrite = req.param('Overwrite');
+	var sql;
 	async.series([
 	        function(callback){
+
+			if(Overwrite == 0){
+				sql = 'insert into XaiMarsh.fp_world_state values ('+IDNumber+', '+Firstname+', '+Lastname+', 0)';
+			} else {
+				sql = 'update XaiMarsh.fp_world_state set Firstname = '+Firstname+', Lastname = '+Lastname+', Dotw = 0 where IDNumber = '+IDNumber;
+			}
         	        connection.query(sql, function(err,row,fields){
                 	        if(err){console.log("We have an error:");
                         	        console.log(err);}
 	                                callback();
                         });
                 },
+		function(callback){
+			if(Overwrite == 0){
+				sql = 'insert into XaiMarsh.fp_stats values ('+IDNumber+', 10, 10, 10, 10, 10, 10)';
+			} else {
+				sql = 'update XaiMarsh.fp_stats set FunFact = 10, Knowledge = 10, CommitProficiency = 10, CodeQuality = 10, MaxEnergy = 10, GoogleProficiency = 10 where IDNumber = '+IDNumber;
+			}
+                        connection.query(sql, function(err,row,fields){
+                                if(err){console.log("We have an error:");
+                                        console.log(err);}
+                                callback();
+                        });
+                },
                 function(callback){
-	                sql = 'update XaiMarsh.till_inventory set amount='+newQuantity+' where id='+invID;
+	                if(Overwrite == 0){
+				sql = 'insert into XaiMarsh.fp_grades values ('+IDNumber+', 75, 75, 75, 75)';
+			} else {
+				sql = 'update XaiMarsh.fp_grades set grade1 = 75, grade2 = 75, grade3 = 75, grade4 = 75 where IDNumber = '+IDNumber;
+			}
                         connection.query(sql, (function(res){return function(err,rows,fields){
                                         if(err){console.log("We have an error:");
                                                 console.log(err);}
@@ -107,7 +126,7 @@ app.get("/newCharacter",function(req,res){
                                         callback();
                                 }})(res));
                        }
-                ]);
+	]);
 });
 
 app.get("/getCharInfo",function(req,res){
@@ -125,29 +144,23 @@ app.get("/getCharInfo",function(req,res){
                 }
        ]);
 });
-app.get("/getCharInventory",function(req,res){
-        var IDNumber = req.param('userID');
-        var sql ='SELECT XaiMarsh.fp_inventory.item, XaiMarsh.fp_inventory.quantity from XaiMarsh.inventory where XaiMarsh.inventory.IDNumber='+IDNumber+')'; 
-        async.series([ 
-                function(callback){ 
-                        connection.query(sql, (function(res){return function(err,rows,fields){
-                                        if(err){console.log("We have an error:");
-                                                console.log(err);}
-                                        res.send(rows);
-                                        callback();
-                                }})(res));
-                       }
-                ]);
-});
+//app.get("/getCharInventory",function(req,res){
+//        var IDNumber = req.param('userID');
+//        var sql ='SELECT XaiMarsh.fp_inventory.item, XaiMarsh.fp_inventory.quantity from XaiMarsh.inventory where XaiMarsh.inventory.IDNumber='+IDNumber+')'; 
+//        async.series([ 
+//                function(callback){ 
+//                        connection.query(sql, (function(res){return function(err,rows,fields){
+//                                        if(err){console.log("We have an error:");
+//                                                console.log(err);}
+//                                        res.send(rows);
+//                                        callback();
+//                                }})(res));
+//                       }
+//                ]);
+//});
 
 app.get("/world_state",function(req,res){
-        var invID = req.param('invID');
-        var quantity = req.param('quantity');
-        var receiptNumber = req.param('receiptNumber');
         var user = req.param('user');
-        var firstTime = req.param('firstTime');
-        var lastTime = req.param('lastTime');
-        var finalCost = req.param('finalCost');
         var sql = 'insert into XaiMarsh.till_sales values('+receiptNumber+', '+invID+', '+quantity+', '+firstTime+', '+lastTime+')';
         var newQuantity = 0;
         if(invID != -1){
@@ -327,7 +340,68 @@ app.get("/getCurrentChanceClassEvent",function(req,res){
         }})(res));
 });
 
+app.get("/getResultEvent",function(req,res){
+        var eventID = req.param('eventID');
+        var sql = 'SELECT * FROM XaiMarsh.fp_result_events where eventID='+eventID;
+        connection.query(sql,(function(res){return function(err,rows,fields){
+        if(err){console.log("We have an error:");
+                console.log(err);}
+        res.send(rows);
+        }})(res));
+});
 
+app.get("/updateDatabase",function(req,res){
+        var IDNumber = req.param('IDNumber');
+	
+	//world_state
+	var Dotw = req.param('Dotw');
+
+	//stats
+	var FunFact = req.param('FunFact');
+	var Knowledge = req.param('Knowledge');
+	var CommitProficiency = req.param('CommitProficiency');
+	var CodeQuality = req.param('CodeQuality');
+	var MaxEnergy = req.param('MaxEnergy');
+	var GoogleProficiency = req.param('GoogleProficiency');
+
+	//grades
+	var grade1 = req.param('grade1');
+	var grade2 = req.param('grade2');
+	var grade3 = req.param('grade3');
+	var grade4 = req.param('grade4');
+   
+       // var sql = 'SELECT XaiMarsh.fp_world_state.Firstname,XaiMarsh.fp_world_state.Lastname,XaiMarsh.fp_world_state.Dotw,XaiMarsh.fp_stats.FunFact,XaiMarsh.fp_stats.Knowledge,XaiMarsh.fp_stats.CommitProficiency,XaiMarsh.fp_stats.CodeQuality,XaiMarsh.fp_stats.MaxEnergy,XaiMarsh.fp_stats.GoogleProficiency,XaiMarsh.fp_grades.grade1,XaiMarsh.fp_grades.grade2,XaiMarsh.fp_grades.grade3,XaiMarsh.fp_grades.grade4 from XaiMarsh.fp_world_state LEFT JOIN XaiMarsh.fp_stats ON XaiMarsh.fp_world_state.IDNumber=XaiMarsh.fp_stats.IDNumber LEFT JOIN XaiMarsh.fp_grades ON XaiMarsh.fp_world_state.IDNumber=XaiMarsh.fp_grades.IDNumber';
+
+	var sql = 'update XaiMarsh.world_state set Dotw = '+Dotw+' where XaiMarsh.world_state.IDNumber = '+IDNumber;
+	async.series([
+                function(callback){
+                        connection.query(sql, function(err,row,fields){
+                                if(err){console.log("We have an error:");
+                                        console.log(err);}
+                                        callback();
+                        });
+                },
+		function(callback){
+			sql = 'update XaiMarsh.stats set FunFact = '+FunFact+', Knowledge = '+Knowledge+', CommitProficiency = '+CommitProficiency+', CodeQuality = '+CodeQuality+', MaxEnergy = '+MaxEnergy+', GoogleProficiency = '+GoogleProficiency+' where IDNumber = '+IDNumber;
+                        connection.query(sql, function(err,row,fields){
+                                if(err){console.log("We have an error:");
+                                        console.log(err);}
+                                        callback();
+                        });
+                },
+                function(callback){
+                        sql = 'update XaiMarsh.grades set grade1 = '+grade1', grade2 = '+grade2+', grade3 = '+grade3+', grade4 = '+grade4+' where IDNumber='IDNumber;
+                        connection.query(sql, (function(res){return function(err,rows,fields){
+                                        if(err){console.log("We have an error:");
+                                                console.log(err);}
+                                    res.send(err);
+                                        callback();
+                                }})(res));
+                       }
+        ]);
+
+
+});
 
 
 
